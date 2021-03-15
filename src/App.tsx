545faa8bc75fc84b45e359fa7387829ef55c847e
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-15 11:49:41 -0300

    feat: import dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 
 export function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Dashboard />
     </>
   );
 }
