a4d8c460347e0c348e78b92857cc9731a976aad0
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-15 11:13:48 -0300

    feat: import globalstyles and header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>Hellow World!</h1>
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
