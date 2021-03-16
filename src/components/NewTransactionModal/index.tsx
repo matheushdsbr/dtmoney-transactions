451997ef90ab3f1e1187a2f71cd4bcf397d54729
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-16 10:17:54 -0300

    feat: add transactionstypecontainer

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionsTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -28,7 +30,19 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
         <input placeholder="Título" />
 
-        <input placeholder="Valor" type="number" />
+        <input type="number" placeholder="Valor" />
+
+        <TransactionsTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída"/>
+            <span>Saída</span>
+          </button>
+        </TransactionsTypeContainer>
 
         <input placeholder="Categoria" />
 
