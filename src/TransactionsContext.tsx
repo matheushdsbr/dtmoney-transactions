7f096c8bb7a34ed228f866cd5a131d2a0245ceb8
Matheus Henrique <32910717+matheushenrique1@users.noreply.github.com>
2021-03-29 13:57:50 -0300

    feat: add api

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "./services/api";
 
-interface TransactionsProps {
+interface Transaction {
   id: number;
   title: string;
   amount: number;
@@ -10,14 +10,23 @@ interface TransactionsProps {
   createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
 interface TransactionsProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<TransactionsProps[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
-  const [transactions, setTransactions] = useState<TransactionsProps[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
     api
@@ -25,8 +34,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post("/transactions", transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   );
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,33 +1,39 @@
-import { FormEvent, useState } from 'react';
-import Modal from 'react-modal';
-import closeImg from '../../assets/close.svg';
-import incomeImg from '../../assets/income.svg';
-import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
-import { Container, TransactionsTypeContainer, RadioBox } from './styles';
+import { FormEvent, useState, useContext } from "react";
+import Modal from "react-modal";
+
+import { TransactionsContext } from "../../TransactionsContext";
+
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
+import { Container, TransactionsTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
-  const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
-  const [category, setCategory] = useState('');
-  const [type, setType] = useState('deposit');
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("deposit");
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    createTransaction({
       title,
-      value,
+      amount,
       category,
-      type
-    };
-
-    api.post('/transactions', data);
+      type,
+    });
   }
 
   return (
@@ -42,7 +48,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         onClick={onRequestClose}
         className="react-modal-close"
       >
-        <img src={closeImg} alt="Fechar modal"/>
+        <img src={closeImg} alt="Fechar modal" />
       </button>
 
       <Container onSubmit={handleCreateNewTransaction}>
@@ -51,34 +57,38 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <input
           placeholder="Título"
           value={title}
-          onChange={event => setTitle(event.target.value)}
+          onChange={(event) => setTitle(event.target.value)}
         />
 
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={(event) => setAmount(Number(event.target.value))}
         />
 
         <TransactionsTypeContainer>
           <RadioBox
             type="button"
-            onClick={() => { setType('deposit'); }}
-            isActive={type === 'deposit'}
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
             activeColor="green"
           >
-            <img src={incomeImg} alt="Entrada"/>
+            <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
           </RadioBox>
 
           <RadioBox
             type="button"
-            onClick={() => { setType('withdraw'); }}
-            isActive={type === 'withdraw'}
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
             activeColor="red"
           >
-            <img src={outcomeImg} alt="Saída"/>
+            <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
           </RadioBox>
         </TransactionsTypeContainer>
@@ -86,7 +96,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <input
           placeholder="Categoria"
           value={category}
-          onChange={event => setCategory(event.target.value)}
+          onChange={(event) => setCategory(event.target.value)}
         />
 
         <button type="submit">Cadastrar</button>
